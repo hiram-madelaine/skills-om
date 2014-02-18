66c1dc1b5782dad0a72ae9895f66e3648c970dff
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2014-02-18 20:42:24 +0100

    On peut ajouter des skills

--- src/skills_om/core.cljs
+++ src/skills_om/core.cljs
@@ -1,33 +1,91 @@
 (ns skills-om.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [chan put! <!]]
             [clojure.set :as s]))
 
 (enable-console-print!)
 
-(def app-state (atom [{:id 0 :label "Clojure" :version "1.6" :level 4 :tier :middle :cat :lang}
-                      {:id 1 :label "Spring" :version "3.0.0" :level 3 :tier :middle :cat :frmk}
-                     {:id 2 :label "Java" :version "7" :level 4 :tier :middle :cat :lang}
-                     {:id 3 :label "Oracle" :version "7" :level 5 :tier :data :cat :server}
+
+;;;;; App State ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def app-state (atom [{:id 0 :label "Clojure" :version "1.6" :level 4 :tier "middle" :cat "lang"}
+                      {:id 1 :label "Spring" :version "3.0.0" :level 3 :tier "middle" :cat "frmk"}
+                     {:id 2 :label "Java" :version "7" :level 4 :tier "middle" :cat "lang"}
+                     {:id 3 :label "Oracle" :version "7" :level 5 :tier "data" :cat "server"}
                       ]))
-(swap! app-state conj {:id 4 :label "Weblogic" :level 4 :version "10.3" :cat :server :tier :middle})
+#_(swap! app-state conj {:id 4 :label "Weblogic" :level 4 :version "10.3" :cat "server" :tier "middle"})
 
-#_[
-         {:tier :front :cat :lang :label "langage" :skills [ {:label "HTML" :level 4 :version 5}{:label "CSS" :level 3 :version 3}{:label "Javascript" :level 3}]}
-         {:tier :front :cat :frmk :label "Framework" :skills [{:label "DWR" :level 3 :version 2}{:label "JQuery"}]}
-         {:tier :middle :cat :lang :label "Language" :skills [{:label "Java" :level 5} {:label "Groovy" :level 3} {:label "Clojure" :level 2} {:label "Scala" :level 2}]}
-         {:tier :middle :cat :frmk :label "Framework" :skills [{:label "Spring Core" :level 4}{:label "Hibernate" :level 4}{:label "Camel" :version "2.7" :level 4}]}
-         {:tier :middle :cat :lib :label "Librairie" :skills [{:label "Google-Collections" :level 4}{:label "Joda" :level 4 :version "2.0"}{:label "POI" :version "3.7" :level 4}]}
-         {:tier :middle :cat :server :label "Serveurs" :skills [{:label "Weblogic" :level 4 :version "10.3"} {:label "Tomcat" :version "6" :level 3}{:label "GlassFish" :version 2 :level 3}]}
-         {:tier :data :cat :server :label "Base de donnÃ©es" :skills [ {:label "Oracle" :level 4 :version "10g"}{:label "PostgreSql" :level 3} {:label "mySQL" :level 2}{:label "MongoDB" :level 2}]}
-         ]
 
+
+(def app-ref {:tier [{:code "middle" :label "Middleware"}
+                     {:code "front" :label "Front"}
+                     {:code "data" :label "Database"}]
+              :cat [ {:code "lang" :label "Language"}
+                     {:code "frmk" :label "Framework"}
+                     {:code "server" :label "Serveur"}]})
+
+
+;;;;;;;;;; Utils ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn trace
+  "Display the raw data."
   [app owner]
   (om/component
    (dom/p nil (str app))))
+;;;;;; Skill input ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+(defn make-select
+  [c l k v data]
+  (dom/div nil
+           (dom/label nil l)
+           (apply dom/select #js {:value v
+                                       :onChange #(put! c [k (-> % .-target .-value)])}
+                       (dom/option #js {:value ""} "")
+                       (map (fn [{:keys [code label]}]
+                              (dom/option #js {:value code} label)) data))))
+
+(defn make-input
+  [c l k v t]
+  (dom/div nil
+           (dom/label nil l)
+           (dom/input #js {:type t
+                           :value v
+                           :onChange #(put! c [k (-> % .-target .-value)])})))
+
+(defn skill-input
+  [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+                {:chan (chan)
+                 :inputs   {:label ""
+                            :level 0
+                            :cat ""
+                            :tier ""
+                            :version ""}})
+    om/IWillMount
+    (will-mount [this]
+                (let [chan (om/get-state owner :chan)]
+                  (go
+                   (loop []
+                     (let [[k v] (<! chan)]
+                       (om/set-state! owner [:inputs k] v))
+                     (recur)))))
+    om/IRenderState
+    (render-state [_ {chan :chan  {:keys [label level cat tier version] :as new-skill} :inputs }]
+                  (prn new-skill)
+                  (dom/div nil
+                           (make-input chan "label" :label label "text")
+                           (make-input chan "Version" :version version "tex")
+                           (make-input chan "Level" :level level "range")
+                           (make-select chan "Tier" :tier tier (om/get-shared owner [:tier] ) )
+                           (make-select chan "Category" :cat cat (om/get-shared owner [:cat]))
+                           (dom/button #js {:onClick #(om/transact! app (fn [skills] (conj skills new-skill )))})))))
+
+
+;;;;;; Skills Board ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn skill-view
   "Display a skill on a post-it line"
@@ -44,8 +102,8 @@
   (reify
     om/IRender
     (render [_]
-            (dom/article #js {:className (name (:cat cat))}
-                         (dom/h2 nil (dom/p nil (name  (:cat cat))))
+            (dom/article #js {:className (:cat cat)}
+                         (dom/h2 nil (dom/p nil (:cat cat)))
                          (apply dom/ul nil (om/build-all skill-view skills {:key :id}))))))
 
 
@@ -64,7 +122,7 @@
     om/IRender
     (render [_]
             (dom/section nil
-                         (dom/h2 nil (dom/p nil (name (:tier tier))))
+                         (dom/h2 nil (dom/p nil (:tier tier)))
                          (om/build posts-view skills {:fn #(s/index % [:cat])})  ))))
 
 (defn board-view
@@ -80,10 +138,13 @@
   (reify
     om/IRender
     (render [_]
-           (dom/div nil (om/build board-view app {:fn #(s/index % [:tier])})) )))
+           (dom/div nil
+                    (om/build skill-input app)
+                    (om/build board-view app {:fn #(s/index % [:tier])})) )))
 
 
 (om/root
  app-view
  app-state
-  {:target (. js/document (getElementById "app")) })
+  {:target (. js/document (getElementById "app"))
+   :shared app-ref})
