95469a99b8626539136a6dc9388d33ba7ab60a6f
HIRAM MADELAINE <hiram.madelaine@gmail.com>
2014-02-21 10:54:58 +0100

    Om version 0.5.0

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject skills-om "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
+  :description "Display and manage your skills on a Scrum Board"
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2156"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.0-rc1"]
-                 #_[com.facebook/react "0.8.0.1"]]
+                 [om "0.5.0"] ]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
